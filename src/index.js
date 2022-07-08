a83730c5a2cd16a7202365564cfff9301ff66d2a
Michael Duberstein <dubermic@flutterint.com>
2022-07-08 16:31:00 -0400

    Component: After Clip 37 Showing Custom Children, containing various child components and HTML elements in a React component

--- src/index.js
+++ src/index.js
@@ -7,27 +7,37 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45 PM"
-        content="Nice blog post"
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Today at 2:00 AM"
-        content="I Like the subject"
-        avatar={faker.image.image()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yestoday at 5:00 PM"
-        content="I like the writing"
-        avatar={faker.image.image()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45 PM"
+          content="Nice blog post"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00 AM"
+          content="I Like the subject"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yestoday at 5:00 PM"
+          content="I like the writing"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
-
   );
 };
 
